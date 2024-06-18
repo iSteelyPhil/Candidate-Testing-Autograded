31152f1f3ca333d63552ed8807c24b71bc216199
Phil <philmography2010@gmail.com>
2024-06-18 13:26:10 -0500

    part 1

--- candidate-testing-ORIGINAL.js
+++ candidate-testing-ORIGINAL.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space? ";
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
+let question;
+let correctAnswer;
+let candidateAnswer;
 
 
 //TODO: Variables for Part 2
@@ -16,26 +16,23 @@ let correctAnswers;
 let candidateAnswers;
 
 
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  let candidateName = input.question("Please Type Your Name Here");
+
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question)
+
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer){
-    console.log("That is correct!")
-  } else {
-    console.log("That's incorrect!")
-  }
+
+
+
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
@@ -45,7 +42,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello,", candidateName);
+   console.log();
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
