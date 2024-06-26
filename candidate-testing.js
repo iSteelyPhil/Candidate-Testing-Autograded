aa8f194ffe0dc7f828e7c8630c6360aeeada0ae1
Phil <philmography2010@gmail.com>
2024-06-26 18:45:13 -0500

    syntax edit

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,7 +14,7 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = []
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -25,7 +25,7 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = input.question(questions[i])
+    candidateAnswers[i] = input.question(questions[i]);
   }
 }
 
@@ -40,7 +40,7 @@ function gradeQuiz(candidateAnswers) {
     }
   }
 console.log("==========AND NOW FOR YOUR GRADE!!!!==========")
-let numCorrectCanAnswers = 0
+let numCorrectCanAnswers = 0;
 for (let i = 0; i < candidateAnswers.length; i++) {
   if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
      numCorrectCanAnswers++;
